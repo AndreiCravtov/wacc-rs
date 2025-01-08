cd4b9f9c379d445b2986aa82009a0357d5a1addd
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-01-08 04:36:12 +0000

    rewritten lexer but nicer

--- syntax/src/token.rs
+++ syntax/src/token.rs
@@ -1,9 +1,11 @@
 use crate::source::{SourceId, WithSourceId};
+use crate::CharExt;
 use chumsky::error::Rich;
 use chumsky::extra::ParserExtra;
 use chumsky::input::WithContext;
-use chumsky::prelude::{any, Input};
-use chumsky::{IterParser, Parser};
+use chumsky::prelude::{any, choice, end, just, regex, skip_then_retry_until, Input};
+use chumsky::{text, IterParser, Parser};
+use internment::ArcIntern;
 use std::fmt;
 use std::fmt::Formatter;
 
@@ -19,30 +21,30 @@ pub enum Token<'src> {
     Ident(&'src str),
     IntLiter(i32),
     CharLiter(char),
-    StringLiter(&'src str),
+    StringLiter(ArcIntern<str>),
 
-    // delimiters
+    // delimiter symbols
     Open(Delim),
     Close(Delim),
 
     // other symbols
-    Lt,
-    Gt,
     Lte,
+    Lt,
     Gte,
+    Gt,
+    BangEquals,
+    Bang,
+    EqualsEquals,
+    Equals,
+    Plus,
+    Minus,
+    Star,
+    Percent,
+    ForwardSlash,
     And,
     Or,
     Semicolon,
     Comma,
-    Star,
-    Percent,
-    Plus,
-    Minus,
-    ForwardSlash,
-    Bang,
-    Equals,
-    BangEquals,
-    EqualsEquals,
 
     // keywords
     Begin,
@@ -94,23 +96,23 @@ impl fmt::Display for Token<'_> {
                 Delim::Paren => write!(f, ")"),
                 Delim::Bracket => write!(f, "]"),
             },
-            Token::Lt => write!(f, "<"),
-            Token::Gt => write!(f, ">"),
             Token::Lte => write!(f, "<="),
+            Token::Lt => write!(f, "<"),
             Token::Gte => write!(f, ">="),
+            Token::Gt => write!(f, ">"),
+            Token::BangEquals => write!(f, "!="),
+            Token::Bang => write!(f, "!"),
+            Token::EqualsEquals => write!(f, "=="),
+            Token::Equals => write!(f, "="),
+            Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Star => write!(f, "*"),
+            Token::Percent => write!(f, "%"),
+            Token::ForwardSlash => write!(f, "/"),
             Token::And => write!(f, "&&"),
             Token::Or => write!(f, "||"),
             Token::Semicolon => write!(f, ";"),
             Token::Comma => write!(f, ","),
-            Token::Star => write!(f, "*"),
-            Token::Percent => write!(f, "%"),
-            Token::Plus => write!(f, "+"),
-            Token::Minus => write!(f, "-"),
-            Token::ForwardSlash => write!(f, "/"),
-            Token::Bang => write!(f, "!"),
-            Token::Equals => write!(f, "="),
-            Token::BangEquals => write!(f, "!="),
-            Token::EqualsEquals => write!(f, "=="),
             Token::Begin => write!(f, "begin"),
             Token::End => write!(f, "end"),
             Token::Is => write!(f, "is"),
@@ -161,8 +163,134 @@ where
         Error = Rich<'src, char, SourcedLexerSpan<'src, S>>,
     >,
 {
-    any()
-        .map_with(|t, extra| (Token::Null, extra.span()))
+    // WACC identifiers are C-style, so we can use the default `text::ident` parser
+    let ident = text::ident().map(|id| Token::Ident(id));
+
+    // copy the Regex pattern found in the WACC spec verbatim
+    let int_liter = regex("[\\+-]?[0-9]+")
+        .try_map(|s: &str, span| match s.parse::<i32>() {
+            Ok(i) => Ok(i),
+            Err(_) => Err(Rich::custom(
+                span,
+                format!("The integer literal '{}' does not fit within 32 bytes", s),
+            )),
+        })
+        .map(Token::IntLiter);
+
+    // character parser
+    let character = choice((
+        any::<_, E>().filter(char::normal_wacc_char),
+        just::<char, _, E>('\\')
+            .ignore_then(any())
+            .filter(char::escaped_wacc_char),
+    ));
+
+    // character literal parser
+    let char_delim = just::<char, _, E>('\'');
+    let char_liter = character
+        .delimited_by(char_delim, char_delim)
+        .map(Token::CharLiter);
+
+    // string literal parser
+    let string_delim = just::<char, _, E>('"');
+    let string_liter = character
+        .repeated()
+        .collect::<String>()
+        .delimited_by(string_delim, string_delim)
+        .map(ArcIntern::from)
+        .map(Token::StringLiter);
+
+    let delim_symbols = choice((
+        just('(').to(Token::Open(Delim::Paren)),
+        just(')').to(Token::Close(Delim::Paren)),
+        just('[').to(Token::Open(Delim::Bracket)),
+        just(']').to(Token::Close(Delim::Bracket)),
+    ));
+
+    let other_symbols = choice((
+        // if some symbols are ambiguous, make sure to place the more 'long' ones ahead
+        just("<=").to(Token::Lte),
+        just('<').to(Token::Lt),
+        just(">=").to(Token::Gte),
+        just('>').to(Token::Gt),
+        just("!=").to(Token::BangEquals),
+        just('!').to(Token::Bang),
+        just("==").to(Token::EqualsEquals),
+        just('=').to(Token::Equals),
+        // symbols without ambiguity
+        just('+').to(Token::Plus),
+        just('-').to(Token::Minus),
+        just('*').to(Token::Star),
+        just('%').to(Token::Percent),
+        just('/').to(Token::ForwardSlash),
+        just("&&").to(Token::And),
+        just("||").to(Token::Or),
+        just(';').to(Token::Semicolon),
+        just(',').to(Token::Comma),
+    ));
+
+    let keywords = choice([
+        text::keyword("begin").to(Token::Begin),
+        text::keyword("end").to(Token::End),
+        text::keyword("is").to(Token::Is),
+        text::keyword("skip").to(Token::Skip),
+        text::keyword("read").to(Token::Read),
+        text::keyword("free").to(Token::Free),
+        text::keyword("return").to(Token::Return),
+        text::keyword("exit").to(Token::Exit),
+        text::keyword("print").to(Token::Print),
+        text::keyword("println").to(Token::Println),
+        text::keyword("if").to(Token::If),
+        text::keyword("then").to(Token::Then),
+        text::keyword("else").to(Token::Else),
+        text::keyword("fi").to(Token::Fi),
+        text::keyword("while").to(Token::While),
+        text::keyword("do").to(Token::Do),
+        text::keyword("done").to(Token::Done),
+        text::keyword("newpair").to(Token::Newpair),
+        text::keyword("pair").to(Token::Pair),
+        text::keyword("fst").to(Token::Fst),
+        text::keyword("snd").to(Token::Snd),
+        text::keyword("call").to(Token::Call),
+        text::keyword("int").to(Token::Int),
+        text::keyword("bool").to(Token::Bool),
+        text::keyword("char").to(Token::Char),
+        text::keyword("string").to(Token::String),
+        text::keyword("len").to(Token::Len),
+        text::keyword("ord").to(Token::Ord),
+        text::keyword("chr").to(Token::Chr),
+        text::keyword("null").to(Token::Null),
+        text::keyword("true").to(Token::True),
+        text::keyword("false").to(Token::False),
+    ]);
+
+    let token = choice((
+        // parser literals first, as they have precedence over any other occurrences of symbols
+        // e.g. the literal +14343 should take precedence over the plus symbol '+'
+        int_liter,
+        char_liter,
+        string_liter,
+        // parse symbols
+        delim_symbols,
+        other_symbols,
+        // first parse keywords, only then parse identifiers as a fallback
+        keywords,
+        ident,
+    ));
+
+    // the comments are only single-line, started with a hashtag
+    let comment = just("#")
+        .then(any().and_is(just('\n').not()).repeated())
+        .padded();
+
+    token
+        .map_with(|t, e| (t, e.span()))
+        .padded_by(comment.repeated())
+        .padded()
+        // If we encounter an error, skip and attempt to lex the next character as a token instead
+        .recover_with(skip_then_retry_until(any().ignored(), end()))
         .repeated()
         .collect()
+        // We must consume the entire source at the end
+        .then_ignore(end())
 }
